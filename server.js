2aa4bfdba385af28149a11237a3a39e4e61d1f9b
Andrew <webdev410@gmail.com>
2024-06-19 14:46:19 -0500

    port

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-const PORT = process.env.NODE_ENV === 'dev' ? 3001 : 6000;
+// const PORT = 3001; // bring this in when developing
+const PORT = 6000;
 
 app.use(express.static('./public'));
 
